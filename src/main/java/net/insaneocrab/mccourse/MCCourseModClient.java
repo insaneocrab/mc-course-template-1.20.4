add334b88260069cdd7bfa1a6ea4f21fd9292d9e
John Lambert <jlambe61@students.kennesaw.edu>
2024-02-07 20:50:22 -0500

    Initial commit

---
+++ src/main/java/net/insaneocrab/mccourse/MCCourseModClient.java
@@ -0,0 +1,10 @@
+package net.insaneocrab.mccourse;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MCCourseModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
